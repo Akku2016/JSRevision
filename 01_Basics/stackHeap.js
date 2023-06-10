926de87434a2c0b9e028914b2fdca3089634fadd
Akku2016 <88783468+Akku2016@users.noreply.github.com>
2023-06-10 16:23:38 +0000

    stack and heap

---
+++ 01_Basics/stackHeap.js
@@ -0,0 +1,18 @@
+let name="john";
+let newName =name;
+ newName= "fg";
+console.log(newName);
+console.log(name);
+let user ={
+    name:"fggh",
+    email:"gh@gmail.com"
+}
+let user1 =user
+ user1.name="fh"
+ user1.email="ggh@gmail.com"
+
+
+
+console.log(user1);
+console.log(user);
+
