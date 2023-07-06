4de62407a93280a3a7ba14526b5fa1ace424f16b
Akku2016 <88783468+Akku2016@users.noreply.github.com>
2023-07-06 06:00:54 +0000

    string and number and math

---
+++ 01_Basics/numandmath.js
@@ -0,0 +1,12 @@
+const balance = new Number(200);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+const othernumber =233.5678;
+console.log(othernumber.toPrecision(3))
+
+console.log(Math.abs(-5))//ye positive value deta h
+console.log(Math.ceil(12.4))
+console.log(Math.floor(1.2))
+
+console.log(Math.random())// this very important 
---
+++ 01_Basics/strings.js
@@ -0,0 +1,9 @@
+const name="Akansha"
+const newString = name.substring(-8,4);
+console.log(newString);
+const anotherString = name.slice(0,-5);
+console.log(anotherString);
+
+const newsentence = "  this is a computer"
+console.log(newsentence.split("",3))
+console.log(newsentence.trim())
