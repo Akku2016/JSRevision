43e3eaaab1df3c9bf387bb7dfc9eb6d5670a4383
Akku2016 <88783468+Akku2016@users.noreply.github.com>
2023-07-08 16:14:17 +0000

    arraymethod

---
+++ Basics/02_Basics/array.js
@@ -0,0 +1,14 @@
+const food = ["pizza","cake","salad","cookie","burger","cgh","hgj"];
+ 
+const news = food.slice(2,4);
+console.log("a",food);
+console.log(news);
+console.log(food);
+const newsplice = food.splice(2,4);
+console.log("b",food);
+console.log(newsplice);
+console.log("c",food);
+const arr =[0,1,2,3,4,5,6]
+const number = arr.splice(2,4);
+console.log("arr is", arr);
+console.log("number is", number);
