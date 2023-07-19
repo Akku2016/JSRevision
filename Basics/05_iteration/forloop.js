9ba1ac9fae832e3839fdb5cbbaaf9c7cf9b4d065
Akku2016 <88783468+Akku2016@users.noreply.github.com>
2023-07-19 05:04:18 +0000

    forloop and forof

---
+++ Basics/05_iteration/forloop.js
@@ -0,0 +1,23 @@
+//for
+for(let i=0; i<10; i++){
+    const element =i;
+    if(element==5){
+        console.log("5 is the best number");
+        //break
+        //continue
+    }
+    console.log(element);
+}
+for(let i=0;i<=10;i++){
+    console.log(`value is i = ${i}`);
+    for(let j=0;j<=10;j++)
+    {
+        console.log(`value is j = ${j}`);
+    }
+}
+
+let myArray=["batman","superman","flash"]
+for(let index=0; index<myArray.length; index++){
+    const element =myArray[index];
+    console.log(element);
+}
---
+++ Basics/05_iteration/forof.js
@@ -0,0 +1,33 @@
+// for of and for in
+//["","",""]
+//[{},{},{}]
+//array example
+// for of is iterate for array,map,string .not for object.
+const arr =[1,2,3,4,5]
+for(const num of arr) {
+    console.log(num);
+}
+
+//string example
+let str ="Hello guys"
+for(const string of str){
+    console.log(string);
+}
+//object example
+let objectexample =
+    {
+        js:"java script",
+        Rs:"Ruby"
+    }
+
+//for(const example of objectexample){
+    //console.log(example);//error it will not work here in "forof"
+//}
+//map example
+const mapexample = new Map()
+mapexample.set('IN', "India")
+mapexample.set('USA', "United States of America")
+
+for(const [key,value] of mapexample){
+    console.log(key ,":-" ,value);
+}
